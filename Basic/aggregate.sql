b3685ba2c7724ca5bcd3250817d085e360bc1d12
Vijay Babu <vb9477998@gmail.com>
2026-04-01 18:51:41 +0530

    Add aggregate SQL queries for employee statistics

---
+++ Basic/aggregate.sql
@@ -0,0 +1,11 @@
+-- Total employees
+SELECT COUNT(*) FROM Employee;
+
+-- Maximum salary
+SELECT MAX(salary) FROM Employee;
+
+-- Minimum salary
+SELECT MIN(salary) FROM Employee;
+
+-- Average salary
+SELECT AVG(salary) FROM Employee;
