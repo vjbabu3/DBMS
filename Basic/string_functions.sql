f941699d429f4789ae9e73f4c3253af0b3772a39
Vijay Babu <vb9477998@gmail.com>
2026-04-15 14:21:25 +0530

    Add SQL queries for string functions on Employee

---
+++ Basic/string_functions.sql
@@ -0,0 +1,8 @@
+-- Convert name to uppercase
+SELECT UPPER(name) FROM Employee;
+
+-- Convert name to lowercase
+SELECT LOWER(name) FROM Employee;
+
+-- Length of name
+SELECT name, LENGTH(name) FROM Employee;
